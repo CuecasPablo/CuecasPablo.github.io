b516308d42c5365fe32a91065fd2da26de9f2f5c
CuecasPablo <cuecaspablo@gmail.com>
2025-05-03 19:06:04 +0200

    Tercer commit. Añade funciones de agregar y borrar
    Añade un array, la función de agregar elementos al array y la función de borrar un elemento del array.

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+
+let nombres = []; // Declara el array nombres.
+let entradaNombre = document.getElementById("entradaNombre"); //Declara la variable entradaNombre. Contiene el input del formulario entradaNombre.
+let botonGuardar = document.getElementById("botonGuardar"); //Declara la variable botonGuardar. Contiene el botón del formulario que guarda los nombres en el array.
+let botonBorrar = document.getElementById("botonBorrar"); // //Declara la variable botonBorrar. Contiene el botón del formulario que elimina nombres del array.
+
+//#### FUNCIÓN PARA AGREGAR NOMBRES AL ARRAY ####
+function agregarNombre() {
+
+    nombres.push(entradaNombre.value); // agrega al final del array el valor de entradaNombre(valor escrito en el formulario).
+    entradaNombre.value = ""; // Después de añadir el valor, entradaNombre se queda vacío para no tener que borrar constantemente cuando se vuelve a añadir.
+    console.log(nombres); // Muestra el array en la consola.
+}
+
+botonGuardar.addEventListener("click", agregarNombre); // Cuando se pulse el botón Guardar se hace la función agregarNombre.
+
+
+// #### FUNCIÓN PARA BORRAR UN NOMBRE DEL ARRAY ####
+function borrarNombre() {
+
+    let nombreBorrar = entradaNombre.value; // Guarda en la variable nombreBorrar el valor del formulario.
+    let indiceBorrar = nombres.indexOf(nombreBorrar); //Busca en el array nombreBorrar y guarda su posición en indiceBorrar. Si es -1 es que no existe.
+
+    if (indiceBorrar !== -1) { //Si indiceBorrar NO es -1.
+        nombres.splice(indiceBorrar, 1); //Elimina del array un elemento a partir del indice indiceBorrar(es decir a él mismo).
+        console.log("Se ha eliminado a: " + nombreBorrar); //Muestra un mensaje en la consola con el nombre eliminado.
+    } else { //Si el indiceBorrar es -1
+        console.log("No se encuentra ese nombre");// Muestra un mensaje en la consola para indicar que el nombre no existe.
+    }
+
+    entradaNombre.value = ""; // Pone en blanco el formulario.
+    console.log(nombres); // Muestra el array por la consola.
+}
+
+botonBorrar.addEventListener("click", borrarNombre); // Cuando se pulse el botón Borrar se hace la función borrarNombre.
+
+
+
