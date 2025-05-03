6ac7edfe4c3231bfda12fe974f011fc7e840c3f1
CuecasPablo <cuecaspablo@gmail.com>
2025-05-03 21:59:22 +0200

    4º commit. Añade borrar todo y mostrar array
    Añade un botón para borrar todo el array y dejarlo vacío. También muestra el array en el propio documento.
    
    Mostrar el array tiene un problema: Desplaza el formulario hacia arriba.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@
 let nombres = []; // Declara el array nombres.
 let entradaNombre = document.getElementById("entradaNombre"); //Declara la variable entradaNombre. Contiene el input del formulario entradaNombre.
 let botonGuardar = document.getElementById("botonGuardar"); //Declara la variable botonGuardar. Contiene el botón del formulario que guarda los nombres en el array.
-let botonBorrar = document.getElementById("botonBorrar"); // //Declara la variable botonBorrar. Contiene el botón del formulario que elimina nombres del array.
+let botonBorrar = document.getElementById("botonBorrar"); // Declara la variable botonBorrar. Contiene el botón del formulario que elimina nombres del array.
+let botonBorrarTodo = document.getElementById("botonBorrarTodo"); //Declara la variable botonBorrarTodo. Contiene el botón del formulario que borra todo el array.
 
 //#### FUNCIÓN PARA AGREGAR NOMBRES AL ARRAY ####
 function agregarNombre() {
@@ -10,6 +11,7 @@ function agregarNombre() {
     nombres.push(entradaNombre.value); // agrega al final del array el valor de entradaNombre(valor escrito en el formulario).
     entradaNombre.value = ""; // Después de añadir el valor, entradaNombre se queda vacío para no tener que borrar constantemente cuando se vuelve a añadir.
     console.log(nombres); // Muestra el array en la consola.
+    mostrarArray()// mostrar el array en el propio documento.
 }
 
 botonGuardar.addEventListener("click", agregarNombre); // Cuando se pulse el botón Guardar se hace la función agregarNombre.
@@ -30,9 +32,27 @@ function borrarNombre() {
 
     entradaNombre.value = ""; // Pone en blanco el formulario.
     console.log(nombres); // Muestra el array por la consola.
+    mostrarArray()// mostrar el array en el propio documento.
 }
 
 botonBorrar.addEventListener("click", borrarNombre); // Cuando se pulse el botón Borrar se hace la función borrarNombre.
+
+// #### FUNCIÓN PARA BORRAR TODO EL ARRAY ####
+function borrarTodoArray(){
+    nombres.splice(0, nombres.length);//Borra desde el índice 0 hasta la logitud completa del array.
+    entradaNombre.value = ""; // Pone en blanco el formulario.
+    console.log(nombres); // Muestra el array por la consola.
+    mostrarArray()// mostrar el array en el propio documento.
+}
+
+botonBorrarTodo.addEventListener("click", borrarTodoArray);// Cuando se pulse el botón borrar todo se hace la función borrarTodoArray.
+
+// #### FUNCIÓN PARA MOSTRAR EL ARRAY ####
+function mostrarArray() {
+    let mostrarNombres = document.getElementById("mostrarArray");
+    mostrarNombres.textContent = nombres; // mostrar el array en el propio documento.
+}
+
 
 
 
