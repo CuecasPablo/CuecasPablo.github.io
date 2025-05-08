f34a7158c7fc8e29193c7c98a16ffb86c8b97c0a
CuecasPablo <cuecaspablo@gmail.com>
2025-05-08 18:43:35 +0200

    9º commit. Cambio en estilos.
    Se ponen botones menos horteras. Se "truca" la función nombreRandom para que no pueda salir mi nombre.

--- js/scripts.js
+++ js/scripts.js
@@ -70,9 +70,8 @@ function mostrarArray() {
         elementoLista.style.fontSize = "30px";
         elementoLista.style.padding = "5px 10px";
         elementoLista.style.backgroundImage = "url('imagenes/fondolista.gif')";
-        elementoLista.style.color = "black";
+        elementoLista.style.color = "darkmagenta";
         elementoLista.style.borderRadius = "5px";
-        elementoLista.style.color = "white";
         elementoLista.style.fontWeight = "bolder";
 
         lista.appendChild(elementoLista);//añade el <li> a la <ul> de mostrarArray.
@@ -88,13 +87,16 @@ function nombreRandom() {
     } else { //si el array tiene nombres.
         let indiceRandom = Math.floor(Math.random() * nombres.length);//genera un numero entre 0 y 0.9^. Lo multiplica por la longitud del array. Lo redondea hacia abajo con Math.floor.
         let nombreSeleccionado = nombres[indiceRandom];//selecciona el nombre correspondiente al indiceRandom.
-        console.log("¡EL/LA AFORTUNAD@ ES " + nombreSeleccionado.toUpperCase() + "!");//muestra en consola el nombre seleccionado.
-        alert("¡EL/LA AFORTUNAD@ ES " + nombreSeleccionado.toUpperCase() + "!");//muestra en un pop-up el nombre seleccionado.
+
+        if(nombreSeleccionado === "Pablo Cuecas"){//si sale mi nombre.
+            console.log("¡" + nombreSeleccionado.toUpperCase() + " NO PUEDE SUSPENDER!");//muestra por consola que yo no puedo suspender.
+            alert("¡" + nombreSeleccionado.toUpperCase() + " NO PUEDE SUSPENDER!")//muestra por pop-up que yo no puedo suspender.
+        } else {//si es otro nombre.
+            console.log("¡EL/LA AFORTUNAD@ ES " + nombreSeleccionado.toUpperCase() + "!");//muestra en consola el nombre seleccionado.
+            alert("¡EL/LA AFORTUNAD@ ES " + nombreSeleccionado.toUpperCase() + "!");//muestra en un pop-up el nombre seleccionado.
+        }
     }
 }
 
 botonSorteo.addEventListener("click", nombreRandom);//cuando se pulsa el botón botonSorteo hace la función nombreRandom.
-
-
-
 
